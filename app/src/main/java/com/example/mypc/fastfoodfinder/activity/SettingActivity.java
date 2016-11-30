a988d0eb1d8f85fc18ffe74d7431cf5cb8b769c5
TranThiNha <tranthinha160296@gmail.com>
2016-11-29 21:28:41 -0700

    change language

--- app/src/main/java/com/example/mypc/fastfoodfinder/activity/SettingActivity.java
+++ app/src/main/java/com/example/mypc/fastfoodfinder/activity/SettingActivity.java
@@ -1,16 +1,21 @@
 package com.example.mypc.fastfoodfinder.activity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mypc.fastfoodfinder.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,13 +24,17 @@ public class SettingActivity extends AppCompatActivity {
 
     @BindView(R.id.tv_setting_sign_out) TextView signOutView;
 
+    @BindView(R.id.sw_languages)
+    SwitchCompat swLanguage;
+
+    @BindView(R.id.tv_setting_english)
+    TextView tvSettingLanguage;
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
         ButterKnife.bind(this);
 
         // Initialize Firebase Auth
@@ -46,5 +55,23 @@ public class SettingActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+        tvSettingLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swLanguage.setChecked(true);
+                loadLanguage("vi");
+            }
+        });
+    }
+    public void loadLanguage(String lang){
+        String languageToLoad = lang;
+        Locale locale = new Locale(languageToLoad);
+        Locale.setDefault(locale);
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getBaseContext().getResources().updateConfiguration(configuration,
+                getBaseContext().getResources().getDisplayMetrics());
     }
 }
