b054f88a349bb86d56f03fce6e25785151ab042a
TranThiNha <tranthinha160296@gmail.com>
2016-12-05 16:05:08 -0700

    Revert "set Icon for list"
    This reverts commit 849c37479e97ed81d9efa13a08dc58e49410c44a.

--- app/src/main/java/com/example/mypc/fastfoodfinder/dialog/DialogCreateNewList.java
+++ app/src/main/java/com/example/mypc/fastfoodfinder/dialog/DialogCreateNewList.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by MyPC on 11/30/2016.
@@ -32,8 +31,6 @@ public class DialogCreateNewList extends DialogFragment {
     EditText edtName;
     OnCreateListListener mListener;
     ArrayList<String> listName;
-    CircleImageView icon1,icon2, icon3, icon4, icon5, icon6, icon7, icon8;
-    int idIconSource = R.drawable.ic_new_list;
 
     public static DialogCreateNewList newInstance() {
         DialogCreateNewList frag = new DialogCreateNewList();
@@ -43,7 +40,7 @@ public class DialogCreateNewList extends DialogFragment {
     }
 
     public interface OnCreateListListener{
-        void OnButtonClick(String name, int idIconSource);
+        void OnButtonClick(String name);
     }
 
     public void setOnButtonClickListener (OnCreateListListener listener){
@@ -57,14 +54,6 @@ public class DialogCreateNewList extends DialogFragment {
         ivQuit = (ImageView) rootView.findViewById(R.id.ivQuit);
         btnDone = (Button) rootView.findViewById(R.id.btnDone);
         edtName = (EditText) rootView.findViewById(R.id.edtName);
-        icon1 = (CircleImageView) rootView.findViewById(R.id.icon1);
-        icon2 = (CircleImageView) rootView.findViewById(R.id.icon2);
-        icon3 = (CircleImageView) rootView.findViewById(R.id.icon3);
-        icon4 = (CircleImageView) rootView.findViewById(R.id.icon4);
-        icon5 = (CircleImageView) rootView.findViewById(R.id.icon5);
-        icon6 = (CircleImageView) rootView.findViewById(R.id.icon6);
-        icon7 = (CircleImageView) rootView.findViewById(R.id.icon7);
-        icon8 = (CircleImageView) rootView.findViewById(R.id.icon8);
         return rootView;
     }
 
@@ -81,7 +70,7 @@ public class DialogCreateNewList extends DialogFragment {
         listName.add("My Save Places");
         listName.add("My Favourite Places");
         listName.add("My Checked in Places");
-        getIconSourceId();
+
         btnDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -98,7 +87,7 @@ public class DialogCreateNewList extends DialogFragment {
                     }
                     if (check)
                     {
-                        mListener.OnButtonClick(edtName.getText().toString(), idIconSource);
+                        mListener.OnButtonClick(edtName.getText().toString());
                         listName.add(edtName.getText().toString());
                         dismiss();
                     }
@@ -107,57 +96,6 @@ public class DialogCreateNewList extends DialogFragment {
         });
 
 
-    }
-
-    public void getIconSourceId(){
-        icon1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                idIconSource = R.drawable.ic_samplelist1;
-            }
-        });
-        icon2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                idIconSource = R.drawable.ic_samplelist2;
-            }
-        });
-        icon3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                idIconSource = R.drawable.ic_samplelist3;
-            }
-        });
-        icon4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                idIconSource = R.drawable.ic_samplelist4;
-            }
-        });
-        icon5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                idIconSource = R.drawable.ic_samplelist5;
-            }
-        });
-        icon6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                idIconSource = R.drawable.ic_samplelist6;
-            }
-        });
-        icon7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                idIconSource = R.drawable.ic_samplelist7;
-            }
-        });
-        icon8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                idIconSource = R.drawable.ic_samplelist8;
-            }
-        });
     }
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
