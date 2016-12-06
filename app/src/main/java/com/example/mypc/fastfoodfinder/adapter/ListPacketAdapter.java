7cffa9e09566b5e3ebb2348aadf61f21529ae22c
TranThiNha <tranthinha160296@gmail.com>
2016-12-06 00:58:31 -0700

    add new list

---
+++ app/src/main/java/com/example/mypc/fastfoodfinder/adapter/ListPacketAdapter.java
@@ -0,0 +1,89 @@
+package com.example.mypc.fastfoodfinder.adapter;
+
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mypc.fastfoodfinder.R;
+import com.example.mypc.fastfoodfinder.model.ListPacket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by MyPC on 12/5/2016.
+ */
+public class ListPacketAdapter extends RecyclerView.Adapter<ListPacketAdapter.ListViewHolder> {
+
+    List<ListPacket> mListPackets;
+
+    public ListPacketAdapter(){
+        mListPackets = new ArrayList<>();
+    }
+
+    public void addListPacket(ListPacket listPacket){
+        mListPackets.add(listPacket);
+        notifyItemRangeInserted(mListPackets.size(),1);
+    }
+
+    @Override
+    public ListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cardview_new_list,parent,false);
+        return new ListViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ListViewHolder holder, int position) {
+        ListPacket listPacket = mListPackets.get(position);
+        holder.tvName.setText(listPacket.getName());
+        holder.cvIcon.setImageResource(listPacket.getIdIconSource());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mListPackets.size();
+    }
+
+    class ListViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.tvNameList)   TextView tvName;
+        @BindView(R.id.iconNewList)  CircleImageView cvIcon;
+        public ListViewHolder(final View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    final int position = getAdapterPosition();
+                    new AlertDialog.Builder(itemView.getContext())
+                            .setTitle(R.string.delete_favourite_location)
+                            .setMessage(R.string.are_you_sure)
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    mListPackets.remove(position);
+                                    notifyDataSetChanged();
+                                    Snackbar.make(itemView, R.string.undo, Snackbar.LENGTH_INDEFINITE).show();
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //do nothing
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                    return true;
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/mypc/fastfoodfinder/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mypc/fastfoodfinder/ui/profile/ProfileFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -19,7 +21,9 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.mypc.fastfoodfinder.R;
+import com.example.mypc.fastfoodfinder.adapter.ListPacketAdapter;
 import com.example.mypc.fastfoodfinder.dialog.DialogCreateNewList;
+import com.example.mypc.fastfoodfinder.model.ListPacket;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -34,25 +38,20 @@ public class ProfileFragment extends Fragment {
     @BindView(R.id.ivCoverImage)  ImageView ivCoverImage;
     @BindView(R.id.btnUpdateCoverImage)  Button btnUpdateCoverImage;
     @BindView(R.id.ivCreate)  CircleImageView civCreate;
-    @BindView(R.id.viewPoint)   CardView viewpoint;
-    @BindView(R.id.viewPoint2)   CardView viewpoint2;
-    @BindView(R.id.viewpoint3)   CardView viewpoint3;
-    @BindView(R.id.viewPoint4)   CardView viewpoint4;
-    @BindView(R.id.viewpoint5)   CardView viewpoint5;
-    @BindView(R.id.cvCreateNew)   CardView cvCreate;
+     @BindView(R.id.cvCreateNew)   CardView cvCreate;
     @BindView(R.id.iv_profile_avatar)   ImageView ivAvatarProfile;
     @BindView(R.id.tvName)   TextView tvName;
     @BindView(R.id.tvEmail)   TextView tvEmail;
-    static int listCount = 3;
-    CircleImageView iconNew;
+    @BindView(R.id.rvListPacket)  RecyclerView rvListPacket;
+    @BindView(R.id.tvNumberList) TextView tvNumberList;
     DialogUpdateCoverImage mDialog;
     DialogCreateNewList mDialogCreate;
     public static ArrayList<String> listName;
-
+    ListPacketAdapter mAdapter;
     // Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
-
+    StaggeredGridLayoutManager mLayoutManager;
 
     public static ProfileFragment newInstance(){
         Bundle extras = new Bundle();
@@ -84,6 +83,10 @@ public class ProfileFragment extends Fragment {
         listName.add("My Favourite Places");
         listName.add("My Checked in Places");
 
+        mAdapter = new ListPacketAdapter();
+        mLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+        rvListPacket.setAdapter(mAdapter);
+        rvListPacket.setLayoutManager(mLayoutManager);
         // Initialize Firebase Auth
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
@@ -117,62 +120,14 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(final View view) {
                 mDialogCreate = DialogCreateNewList.newInstance();
-                mDialogCreate.show(getFragmentManager(),"");
+                mDialogCreate.show(getFragmentManager(), "");
                 mDialogCreate.setOnButtonClickListener(new DialogCreateNewList.OnCreateListListener() {
                     @Override
-                    public void OnButtonClick(String name,int idIconSource) {
-                        LayoutInflater newCardView = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                        CardView cardView = (CardView) newCardView.inflate(R.layout.cardview_new_list,null);
-                        TextView tvName = (TextView) cardView.findViewById(R.id.tvNameList);
-                        tvName.setText(name);
-                        if (listCount==4) {
-                            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(475,378);
-                            params.setMargins(50,1000,0,50);
-                            cvCreate.setLayoutParams(params);
-                            ViewGroup insertPoint = (ViewGroup) viewpoint;
-                            insertPoint.addView(cardView, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                            iconNew = (CircleImageView) insertPoint.findViewById(R.id.iconNewList);
-                            iconNew.setImageResource(idIconSource);
-                        }
-                        else if (listCount==5)
-                        {
-                            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(475,378);
-                            params.setMargins(580,1000,0,50);
-                            cvCreate.setLayoutParams(params);
-                            ViewGroup insertPoint = (ViewGroup) viewpoint2;
-                            insertPoint.addView(cardView, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                            iconNew = (CircleImageView) insertPoint.findViewById(R.id.iconNewList);
-                            iconNew.setImageResource(idIconSource);
-                        }
-                        else if (listCount == 6){
-                            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(475,378);
-                            params.setMargins(50,1420,0,50);
-                            cvCreate.setLayoutParams(params);
-                            ViewGroup insertPoint = (ViewGroup) viewpoint3;
-                            insertPoint.addView(cardView, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                            iconNew = (CircleImageView) insertPoint.findViewById(R.id.iconNewList);
-                            iconNew.setImageResource(idIconSource);
-                        }
-                        else if (listCount == 7){
-                            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(475,378);
-                            params.setMargins(580,1420,0,50);
-                            cvCreate.setLayoutParams(params);
-                            ViewGroup insertPoint = (ViewGroup) viewpoint4;
-                            insertPoint.addView(cardView, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                            iconNew = (CircleImageView) insertPoint.findViewById(R.id.iconNewList);
-                            iconNew.setImageResource(idIconSource);
-                        }
-                        else if (listCount ==8){
-                            cvCreate.setVisibility(View.GONE);
-                            ViewGroup insertPoint = (ViewGroup) viewpoint5;
-                            insertPoint.addView(cardView, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                            iconNew = (CircleImageView) insertPoint.findViewById(R.id.iconNewList);
-                            iconNew.setImageResource(idIconSource);
-                        }
+                    public void OnButtonClick(String name, int idIconSource) {
+                        mAdapter.addListPacket(new ListPacket(name, idIconSource));
+                        tvNumberList.setText("("+String.valueOf(mAdapter.getItemCount())+")");
                     }
-
                 });
-                listCount++;
             }
         });
     }
