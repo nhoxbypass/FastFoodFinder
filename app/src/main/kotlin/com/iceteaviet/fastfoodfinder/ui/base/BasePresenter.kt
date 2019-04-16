9379b67fddbcc1cd5bf37e8489364cc90ac2c0ba
nhoxbypass <12061934+nhoxbypass@users.noreply.github.com>
2019-04-16 23:46:19 +0700

    Add BaseView & BasePresenter

---
+++ app/src/main/kotlin/com/iceteaviet/fastfoodfinder/ui/base/BasePresenter.kt
@@ -0,0 +1,11 @@
+package com.iceteaviet.fastfoodfinder.ui.base
+
+/**
+ * Created by tom on 2019-04-16.
+ */
+
+interface BasePresenter {
+    fun subscribe()
+
+    fun unsubscribe()
+}
---
+++ app/src/main/kotlin/com/iceteaviet/fastfoodfinder/ui/base/BaseView.kt
@@ -0,0 +1,9 @@
+package com.iceteaviet.fastfoodfinder.ui.base
+
+/**
+ * Created by tom on 2019-04-16.
+ */
+
+interface BaseView<T> {
+    var presenter: T
+}
