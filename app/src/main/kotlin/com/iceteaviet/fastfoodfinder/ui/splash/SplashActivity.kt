76cb1a96fa1f6ec45a6df700295ddbd61dee4c65
nhoxbypass <12061934+nhoxbypass@users.noreply.github.com>
2019-04-21 22:33:32 +0700

    Convert splash screen to MVP

--- app/src/main/kotlin/com/iceteaviet/fastfoodfinder/ui/splash/SplashActivity.kt
+++ app/src/main/kotlin/com/iceteaviet/fastfoodfinder/ui/splash/SplashActivity.kt
@@ -6,20 +6,13 @@ import android.os.Handler
 import android.os.Looper
 import com.iceteaviet.fastfoodfinder.App
 import com.iceteaviet.fastfoodfinder.R
-import com.iceteaviet.fastfoodfinder.data.remote.store.model.Store
-import com.iceteaviet.fastfoodfinder.data.remote.user.model.User
 import com.iceteaviet.fastfoodfinder.ui.base.BaseActivity
-import com.iceteaviet.fastfoodfinder.utils.filterInvalidData
-import com.iceteaviet.fastfoodfinder.utils.isValidUserUid
 import com.iceteaviet.fastfoodfinder.utils.openLoginActivity
 import com.iceteaviet.fastfoodfinder.utils.openMainActivity
-import io.reactivex.SingleObserver
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 
 
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity(), SplashContract.View {
+    override lateinit var presenter: SplashContract.Presenter
 
     companion object {
         const val SPLASH_DELAY_TIME = 500
@@ -28,89 +21,52 @@ class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        dataManager = App.getDataManager()
-
-        if (dataManager.getPreferencesHelper().getAppLaunchFirstTime() || dataManager.getPreferencesHelper().getNumberOfStores() == 0) {
-            // Download data from Firebase and store in Realm
-            loadStoresFromServer()
-        } else {
-            if (dataManager.isSignedIn()) {
-                val startTime = System.currentTimeMillis()
-                val uid = dataManager.getCurrentUserUid()
-                if (isValidUserUid(uid)) {
-                    // User still signed in, fetch newest user data from server
-                    // TODO: Support timeout
-                    dataManager.getRemoteUserDataSource().getUser(uid)
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(Schedulers.io())
-                            .subscribe(object : SingleObserver<User> {
-                                override fun onSubscribe(d: Disposable) {
-
-                                }
-
-                                override fun onSuccess(user: User) {
-                                    dataManager.setCurrentUser(user)
-                                    openMainActivityWithDelay(startTime)
-                                }
-
-                                override fun onError(e: Throwable) {
-                                    e.printStackTrace()
-                                    openMainActivityWithDelay(startTime)
-                                }
-                            })
-                }
-            } else {
-                openLoginActivity(this@SplashActivity)
-            }
-        }
+        presenter = SplashPresenter(App.getDataManager(), this)
     }
 
-    private fun openMainActivityWithDelay(startTime: Long) {
+    override fun onResume() {
+        super.onResume()
+        presenter.subscribe()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.unsubscribe()
+    }
+
+    override fun openLoginScreen() {
+        openLoginActivity(this)
+    }
+
+    override fun exit() {
+        finish()
+    }
+
+    override fun openMainActivityWithDelay(startTime: Long) {
         val remainTime = SPLASH_DELAY_TIME - (System.currentTimeMillis() - startTime)
         if (remainTime > 0) {
             Handler(Looper.getMainLooper())
                     .postDelayed({
-                        openMainActivity(this@SplashActivity)
+                        openMainActivity(this)
                     }, remainTime)
         } else {
-            openMainActivity(this@SplashActivity)
+            openMainActivity(this)
         }
     }
 
-    private fun loadStoresFromServer() {
-        dataManager.loadStoresFromServer(this)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(object : SingleObserver<List<Store>> {
-                    override fun onSubscribe(d: Disposable) {
-
-                    }
-
-                    override fun onSuccess(storeList: List<Store>) {
-                        dataManager.getPreferencesHelper().setAppLaunchFirstTime(false)
-                        dataManager.getPreferencesHelper().setNumberOfStores(storeList.size)
-                        dataManager.getLocalStoreDataSource().setStores(filterInvalidData(storeList.toMutableList()))
-
-                        openLoginActivity(this@SplashActivity)
-                    }
-
-                    override fun onError(e: Throwable) {
-                        e.printStackTrace()
-
-                        AlertDialog.Builder(this@SplashActivity)
-                                .setTitle(getString(com.iceteaviet.fastfoodfinder.R.string.title_retry_update_db))
-                                .setMessage(getString(com.iceteaviet.fastfoodfinder.R.string.msg_retry_update_db))
-                                .setPositiveButton(android.R.string.yes) { dialog, which ->
-                                    dialog.dismiss()
-                                    loadStoresFromServer()
-                                }
-                                .setNegativeButton(android.R.string.no) { dialog, which ->
-                                    dialog.dismiss()
-                                    finish()
-                                }
-                                .show()
-                    }
-                })
+    override fun showRetryDialog() {
+        AlertDialog.Builder(this@SplashActivity)
+                .setTitle(getString(R.string.title_retry_update_db))
+                .setMessage(getString(R.string.msg_retry_update_db))
+                .setPositiveButton(android.R.string.yes) { dialog, which ->
+                    dialog.dismiss()
+                    presenter.loadStoresFromServer()
+                }
+                .setNegativeButton(android.R.string.no) { dialog, which ->
+                    dialog.dismiss()
+                    exit()
+                }
+                .show()
     }
 
     override val layoutId: Int
---
+++ app/src/main/kotlin/com/iceteaviet/fastfoodfinder/ui/splash/SplashPresenter.kt
@@ -0,0 +1,86 @@
+package com.iceteaviet.fastfoodfinder.ui.splash
+
+import com.iceteaviet.fastfoodfinder.data.DataManager
+import com.iceteaviet.fastfoodfinder.data.remote.store.model.Store
+import com.iceteaviet.fastfoodfinder.data.remote.user.model.User
+import com.iceteaviet.fastfoodfinder.ui.base.BasePresenter
+import com.iceteaviet.fastfoodfinder.utils.filterInvalidData
+import com.iceteaviet.fastfoodfinder.utils.isValidUserUid
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Created by tom on 2019-04-18.
+ */
+class SplashPresenter : BasePresenter<SplashContract.Presenter>, SplashContract.Presenter {
+
+    val splashView: SplashContract.View
+
+    constructor(dataManager: DataManager, profileView: SplashContract.View) : super(dataManager) {
+        this.splashView = profileView
+        this.splashView.presenter = this
+    }
+
+    override fun subscribe() {
+        if (dataManager.getPreferencesHelper().getAppLaunchFirstTime() || dataManager.getPreferencesHelper().getNumberOfStores() == 0) {
+            // Download data from Firebase and store in Realm
+            loadStoresFromServer()
+        } else {
+            if (dataManager.isSignedIn()) {
+                val startTime = System.currentTimeMillis()
+                val uid = dataManager.getCurrentUserUid()
+                if (isValidUserUid(uid)) {
+                    // User still signed in, fetch newest user data from server
+                    // TODO: Support timeout
+                    dataManager.getRemoteUserDataSource().getUser(uid)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(Schedulers.io())
+                            .subscribe(object : SingleObserver<User> {
+                                override fun onSubscribe(d: Disposable) {
+                                    compositeDisposable.add(d)
+                                }
+
+                                override fun onSuccess(user: User) {
+                                    dataManager.setCurrentUser(user)
+                                    splashView.openMainActivityWithDelay(startTime)
+                                }
+
+                                override fun onError(e: Throwable) {
+                                    e.printStackTrace()
+                                    splashView.openMainActivityWithDelay(startTime)
+                                }
+                            })
+                }
+            } else {
+                splashView.openLoginScreen()
+            }
+        }
+    }
+
+    override fun loadStoresFromServer() {
+        dataManager.loadStoresFromServer()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : SingleObserver<List<Store>> {
+                    override fun onSubscribe(d: Disposable) {
+                        compositeDisposable.add(d)
+                    }
+
+                    override fun onSuccess(storeList: List<Store>) {
+                        dataManager.getPreferencesHelper().setAppLaunchFirstTime(false)
+                        dataManager.getPreferencesHelper().setNumberOfStores(storeList.size)
+                        dataManager.getLocalStoreDataSource().setStores(filterInvalidData(storeList.toMutableList()))
+
+                        splashView.openLoginScreen()
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+
+                        splashView.showRetryDialog()
+                    }
+                })
+    }
+}
