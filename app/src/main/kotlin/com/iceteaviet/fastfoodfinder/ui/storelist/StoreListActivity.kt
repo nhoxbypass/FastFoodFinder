dac7bc09cb7e18b948d44d7e4ed0705e1d695d6d
nhoxbypass <12061934+nhoxbypass@users.noreply.github.com>
2019-04-24 01:11:28 +0700

    Convert StoreListActivity to MVP

--- app/src/main/kotlin/com/iceteaviet/fastfoodfinder/ui/storelist/StoreListActivity.kt
+++ app/src/main/kotlin/com/iceteaviet/fastfoodfinder/ui/storelist/StoreListActivity.kt
@@ -5,22 +5,34 @@ import androidx.annotation.Nullable
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.iceteaviet.fastfoodfinder.App
 import com.iceteaviet.fastfoodfinder.R
+import com.iceteaviet.fastfoodfinder.data.remote.store.model.Store
 import com.iceteaviet.fastfoodfinder.ui.base.BaseActivity
-import com.iceteaviet.fastfoodfinder.utils.getFakeStoreList
 import kotlinx.android.synthetic.main.activity_store_list.*
 
 /**
  * Created by MyPC on 11/30/2016.
  */
-class StoreListActivity : BaseActivity() {
+class StoreListActivity : BaseActivity(), StoreListContract.View {
+    override lateinit var presenter: StoreListContract.Presenter
+
     lateinit var recyclerView: RecyclerView
+
+    override val layoutId: Int
+        get() = R.layout.activity_store_list
 
     private var adapter: StoreListAdapter? = null
 
     override fun onCreate(@Nullable savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        presenter = StoreListPresenter(App.getDataManager(), this)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
         recyclerView = rv_top_list
 
         adapter = StoreListAdapter()
@@ -29,14 +41,19 @@ class StoreListActivity : BaseActivity() {
         recyclerView.adapter = adapter
         val decoration = DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL)
         recyclerView.addItemDecoration(decoration)
-        loadData()
     }
 
-    override val layoutId: Int
-        get() = R.layout.activity_store_list
+    override fun onResume() {
+        super.onResume()
+        presenter.subscribe()
+    }
 
+    override fun onPause() {
+        super.onPause()
+        presenter.unsubscribe()
+    }
 
-    private fun loadData() {
-        adapter!!.setStores(getFakeStoreList())
+    override fun setStores(stores: List<Store>) {
+        adapter!!.setStores(stores)
     }
 }
