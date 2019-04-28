27ef11b6f51df0f1660156a4c6b25887c2bd5a10
nhoxbypass <12061934+nhoxbypass@users.noreply.github.com>
2019-04-29 01:18:53 +0700

    Convert main recently store to MVP

---
+++ app/src/main/kotlin/com/iceteaviet/fastfoodfinder/ui/main/recently/MainRecentlyContract.kt
@@ -0,0 +1,19 @@
+package com.iceteaviet.fastfoodfinder.ui.main.recently
+
+import com.iceteaviet.fastfoodfinder.data.remote.store.model.Store
+import com.iceteaviet.fastfoodfinder.ui.base.BaseView
+import java.util.*
+
+/**
+ * Created by tom on 2019-04-18.
+ */
+interface MainRecentlyContract {
+    interface View : BaseView<Presenter> {
+        fun setStores(stores: ArrayList<Store>)
+        fun showStoreDetailView(store: Store)
+    }
+
+    interface Presenter : com.iceteaviet.fastfoodfinder.ui.base.Presenter {
+        fun onStoreItemClick(store: Store)
+    }
+}
