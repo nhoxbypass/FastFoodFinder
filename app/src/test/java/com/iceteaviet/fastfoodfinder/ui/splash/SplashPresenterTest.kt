10f37cab1239bf79ac56e2101ebc212bc6c5b6f2
nhoxbypass <12061934+nhoxbypass@users.noreply.github.com>
2019-06-05 00:34:48 +0700

    Fix tests

--- app/src/test/java/com/iceteaviet/fastfoodfinder/ui/splash/SplashPresenterTest.kt
+++ app/src/test/java/com/iceteaviet/fastfoodfinder/ui/splash/SplashPresenterTest.kt
@@ -1,6 +1,8 @@
 package com.iceteaviet.fastfoodfinder.ui.splash
 
 import com.iceteaviet.fastfoodfinder.data.DataManager
+import com.iceteaviet.fastfoodfinder.utils.rx.SchedulerProvider
+import com.iceteaviet.fastfoodfinder.utils.rx.TrampolineSchedulerProvider
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -19,14 +21,18 @@ class SplashPresenterTest {
 
     private lateinit var splashPresenter: SplashPresenter
 
+    private lateinit var schedulerProvider: SchedulerProvider
+
     @Before
     fun setupNotesPresenter() {
         // Mockito has a very convenient way to inject mocks by using the @Mock annotation. To
         // inject the mocks in the test the initMocks method needs to be called.
         MockitoAnnotations.initMocks(this)
 
+        schedulerProvider = TrampolineSchedulerProvider()
+
         // Get a reference to the class under test
-        splashPresenter = SplashPresenter(dataManager, splashView)
+        splashPresenter = SplashPresenter(dataManager, schedulerProvider, splashView)
     }
 
     @Test
--- app/src/test/java/com/iceteaviet/fastfoodfinder/ui/store/StoreDetailPresenterTest.kt
+++ app/src/test/java/com/iceteaviet/fastfoodfinder/ui/store/StoreDetailPresenterTest.kt
@@ -2,16 +2,16 @@ package com.iceteaviet.fastfoodfinder.ui.store
 
 import com.iceteaviet.fastfoodfinder.data.DataManager
 import com.iceteaviet.fastfoodfinder.data.remote.routing.model.MapsDirection
-import com.iceteaviet.fastfoodfinder.data.remote.store.model.Comment
 import com.iceteaviet.fastfoodfinder.data.remote.store.model.Store
 import com.iceteaviet.fastfoodfinder.utils.StoreType
 import com.iceteaviet.fastfoodfinder.utils.getFakeComments
-import io.reactivex.SingleObserver
+import com.iceteaviet.fastfoodfinder.utils.rx.TrampolineSchedulerProvider
+import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
-import org.mockito.ArgumentCaptor
-import org.mockito.Captor
+import org.mockito.ArgumentMatchers
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 
@@ -23,53 +23,43 @@ class StoreDetailPresenterTest {
     @Mock
     private lateinit var storeDetailView: StoreDetailContract.View
 
+    private lateinit var storeDetailPresenter: StoreDetailPresenter
+
     @Mock
     private lateinit var dataManager: DataManager
 
-    /**
-     * [ArgumentCaptor] is a powerful Mockito API to capture argument values and use them to
-     * perform further actions or assertions on them.
-     */
-    @Captor
-    private lateinit var getStoreCallbackCaptor: ArgumentCaptor<SingleObserver<List<Comment>>>
-
-    private lateinit var storeDetailPresenter: StoreDetailPresenter
-
     @Before
-    fun setupNotesPresenter() {
+    fun setupPresenter() {
         // Mockito has a very convenient way to inject mocks by using the @Mock annotation. To
         // inject the mocks in the test the initMocks method needs to be called.
         MockitoAnnotations.initMocks(this)
 
         // Get a reference to the class under test
-        storeDetailPresenter = StoreDetailPresenter(dataManager, storeDetailView)
+        storeDetailPresenter = StoreDetailPresenter(dataManager, TrampolineSchedulerProvider(), storeDetailView)
     }
 
     @Test
     fun subscribeTest() {
         val store = Store(STORE_ID, STORE_TITLE, STORE_ADDRESS, STORE_LAT, STORE_LNG, STORE_TEL, STORE_TYPE)
-
         storeDetailPresenter.handleExtras(store)
+
+        `when`(dataManager.getComments(ArgumentMatchers.anyString())).thenReturn(
+                Single.just(comments)
+        )
+
         storeDetailPresenter.subscribe()
 
         verify(storeDetailView).setToolbarTitle(store.title)
 
-        // Then store is loaded from model, callback is captured and progress indicator is shown
         verify(dataManager).getComments(store.id.toString())
-                .subscribe(getStoreCallbackCaptor.capture())
-        //verify(storeDetailView).setProgressIndicator(true)
 
-        // When store is finally loaded
-        getStoreCallbackCaptor.getValue().onSuccess(comments) // Trigger callback
-
-        // Then progress indicator is hidden and title and description are shown in UI
-        //verify(storeDetailView).setProgressIndicator(false)
         verify(storeDetailView).setStoreComments(comments.toMutableList().asReversed())
     }
 
     @Test
     fun clickOnCallButton_showCallScreen() {
         val store = Store(STORE_ID, STORE_TITLE, STORE_ADDRESS, STORE_LAT, STORE_LNG, STORE_TEL, STORE_TYPE)
+        storeDetailPresenter.handleExtras(store)
 
         storeDetailPresenter.onCallButtonClick(store.tel)
 
@@ -80,6 +70,7 @@ class StoreDetailPresenterTest {
     @Test
     fun clickOnNavigationButton_showNavigationScreen() {
         val store = Store(STORE_ID, STORE_TITLE, STORE_ADDRESS, STORE_LAT, STORE_LNG, STORE_TEL, STORE_TYPE)
+        storeDetailPresenter.handleExtras(store)
 
         storeDetailPresenter.onNavigationButtonClick()
 
