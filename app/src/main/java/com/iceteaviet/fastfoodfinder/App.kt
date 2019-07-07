187fc35f535c5775d6e2ff91fb512fee39678bde
nhoxbypass <12061934+nhoxbypass@users.noreply.github.com>
2019-07-07 18:29:48 +0700

    Implement Notification Manager

--- app/src/main/java/com/iceteaviet/fastfoodfinder/App.kt
+++ app/src/main/java/com/iceteaviet/fastfoodfinder/App.kt
@@ -3,12 +3,17 @@ package com.iceteaviet.fastfoodfinder
 import android.annotation.SuppressLint
 import android.content.Context
 import androidx.multidex.MultiDexApplication
+import androidx.work.*
 import com.iceteaviet.fastfoodfinder.data.DataManager
 import com.iceteaviet.fastfoodfinder.location.GoogleLocationManager
 import com.iceteaviet.fastfoodfinder.location.SystemLocationManager
 import com.iceteaviet.fastfoodfinder.service.eventbus.core.IBus
+import com.iceteaviet.fastfoodfinder.service.workers.SyncDatabaseWorker
 import com.iceteaviet.fastfoodfinder.utils.initLogger
 import com.iceteaviet.fastfoodfinder.utils.rx.SchedulerProvider
+import com.iceteaviet.fastfoodfinder.utils.ui.AppNotiManager
+import java.util.concurrent.TimeUnit
+
 
 /**
  * Created by tom on 7/15/18.
@@ -16,6 +21,8 @@ import com.iceteaviet.fastfoodfinder.utils.rx.SchedulerProvider
 class App : MultiDexApplication() {
 
     companion object {
+        private const val SYNC_DB_JOB_TAG = "SYNC_DB_JOB_TAG"
+
         private lateinit var dataManager: DataManager
         private lateinit var schedulerProvider: SchedulerProvider
         private lateinit var bus: IBus
@@ -52,8 +59,27 @@ class App : MultiDexApplication() {
         dataManager = Injection.provideDataManager()
         schedulerProvider = Injection.provideSchedulerProvider()
         bus = Injection.provideEventBus()
+
         GoogleLocationManager.init(getContext())
         SystemLocationManager.init(getContext())
+
+        AppNotiManager.init(getContext())
+
+        scheduleSyncDBWorker()
+    }
+
+    private fun scheduleSyncDBWorker() {
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+        val myWorkBuilder = PeriodicWorkRequest.Builder(SyncDatabaseWorker::class.java, 1, TimeUnit.MINUTES)
+                .setBackoffCriteria(BackoffPolicy.EXPONENTIAL, 1, TimeUnit.HOURS) // Backoff retry after 1 hour
+        //.setConstraints(constraints)
+
+        val myWork = myWorkBuilder.build()
+        WorkManager.getInstance()
+                .enqueueUniquePeriodicWork(SYNC_DB_JOB_TAG, ExistingPeriodicWorkPolicy.KEEP, myWork)
     }
 
     override fun onTerminate() {
---
+++ app/src/main/java/com/iceteaviet/fastfoodfinder/utils/ui/AppNotiManager.kt
@@ -0,0 +1,141 @@
+package com.iceteaviet.fastfoodfinder.utils.ui
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.iceteaviet.fastfoodfinder.R
+import com.iceteaviet.fastfoodfinder.utils.getSplashScreenIntent
+
+/**
+ * Created by tom on 2019-07-07.
+ */
+
+class AppNotiManager private constructor(private val context: Context) : NotiManager {
+    override fun showStoreSyncStatusNotification(message: String, title: String) {
+        val intent = getSplashScreenIntent(context).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+        val notification = makeStatusNotification(message, title, DB_SYNC_NOTIFICATION_CHANNEL_ID,
+                DB_SYNC_NOTIFICATION_CHANNEL_NAME, DB_SYNC_NOTIFICATION_CHANNEL_DESCRIPTION, pendingIntent, appContext, R.drawable.ic_cloud_update_done)
+
+        NotificationManagerCompat.from(context).notify(STORE_DB_SYNC_NOTIFICATION_ID, notification)
+    }
+
+    override fun showStoreSyncProgressStatusNotification(message: String, title: String) {
+        val notification = makeProgressStatusNotification(message, title, DB_SYNC_NOTIFICATION_CHANNEL_ID,
+                DB_SYNC_NOTIFICATION_CHANNEL_NAME, DB_SYNC_NOTIFICATION_CHANNEL_DESCRIPTION, appContext, R.drawable.ic_cloud_update)
+
+        NotificationManagerCompat.from(context).notify(STORE_DB_SYNC_NOTIFICATION_ID, notification)
+    }
+
+
+    /**
+     * Create a Notification that is shown as a heads-up notification if possible.
+     *
+     * @param message Message shown on the notification
+     * @param context Context needed to create Toast
+     */
+    private fun makeStatusNotification(message: String, title: String,
+                                       notificationChannelId: String, notificationChannelName: String,
+                                       notificationChannelDes: String, pendingIntent: PendingIntent?,
+                                       context: Context, iconId: Int = R.drawable.ic_all_store24h_red): Notification {
+
+        // Make a channel if necessary
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(notificationChannelId, notificationChannelName, importance)
+            channel.description = notificationChannelDes
+
+            // Add the channel
+            val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+            notificationManager?.createNotificationChannel(channel)
+        }
+
+        // Create the notification
+        val builder = NotificationCompat.Builder(context, notificationChannelId)
+                .setSmallIcon(iconId)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(LongArray(0))
+                .setAutoCancel(true)
+
+        if (pendingIntent != null)
+            builder.setContentIntent(pendingIntent)
+
+        return builder.build()
+    }
+
+    private fun makeProgressStatusNotification(message: String, title: String,
+                                               notificationChannelId: String, notificationChannelName: String,
+                                               notificationChannelDes: String, context: Context, iconId: Int = R.drawable.ic_all_store24h_red): Notification {
+
+        // Make a channel if necessary
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(notificationChannelId, notificationChannelName, importance)
+            channel.description = notificationChannelDes
+
+            // Add the channel
+            val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+            notificationManager?.createNotificationChannel(channel)
+        }
+
+        // Create the notification
+        val builder = NotificationCompat.Builder(context, notificationChannelId)
+                .setSmallIcon(iconId)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(false)
+                .setProgress(0, 0, true) // display an indeterminate progress bar (a bar that does not indicate percentage complete)
+
+        return builder.build()
+    }
+
+    companion object {
+        private const val DB_SYNC_NOTIFICATION_CHANNEL_NAME = "DB_SYNC_NOTIFICATION_CHANNEL_NAME"
+        private const val DB_SYNC_NOTIFICATION_CHANNEL_DESCRIPTION = "DB_SYNC_NOTIFICATION_CHANNEL_DESCRIPTION"
+        private const val DB_SYNC_NOTIFICATION_CHANNEL_ID = "DB_SYNC_NOTIFICATION_CHANNEL_ID"
+
+        private const val STORE_DB_SYNC_NOTIFICATION_ID = 1
+
+        private lateinit var appContext: Context
+
+        private var instance: AppNotiManager? = null
+
+        fun init(context: Context) {
+            appContext = context.applicationContext
+        }
+
+        fun getInstance(): AppNotiManager {
+            if (instance == null) {
+                synchronized(AppNotiManager::class.java) {
+                    if (instance == null) {
+                        if (!::appContext.isInitialized)
+                            throw IllegalStateException("Call `AppNotiManager.init(Context)` before calling this method.")
+                        else
+                            instance = AppNotiManager(appContext)
+                    }
+                }
+            }
+            return instance!!
+        }
+    }
+}
