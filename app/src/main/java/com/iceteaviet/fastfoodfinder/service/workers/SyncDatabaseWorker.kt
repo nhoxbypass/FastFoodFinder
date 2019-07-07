ec9a462d14574567166e290bdf18502660ad1603
nhoxbypass <12061934+nhoxbypass@users.noreply.github.com>
2019-07-07 18:29:14 +0700

    Implement sync DB worker

---
+++ app/src/main/java/com/iceteaviet/fastfoodfinder/service/workers/SyncDatabaseWorker.kt
@@ -0,0 +1,65 @@
+package com.iceteaviet.fastfoodfinder.service.workers
+
+import android.content.Context
+import androidx.work.RxWorker
+import androidx.work.WorkerParameters
+import com.iceteaviet.fastfoodfinder.App
+import com.iceteaviet.fastfoodfinder.R
+import com.iceteaviet.fastfoodfinder.data.DataManager
+import com.iceteaviet.fastfoodfinder.data.remote.store.model.Store
+import com.iceteaviet.fastfoodfinder.utils.filterInvalidData
+import com.iceteaviet.fastfoodfinder.utils.ui.AppNotiManager
+import com.iceteaviet.fastfoodfinder.utils.ui.NotiManager
+import io.reactivex.Single
+import io.reactivex.SingleObserver
+import io.reactivex.disposables.Disposable
+
+
+/**
+ * Created by tom on 2019-07-07.
+ *
+ * Note that RxWorker.createWork() is called on the main thread, but the return value is subscribed on a background thread by default
+ */
+class SyncDatabaseWorker(ctx: Context, params: WorkerParameters) : RxWorker(ctx, params) {
+
+    private val dataManager: DataManager
+    private val notiManager: NotiManager
+
+    init {
+        // TODO: Inject these dependencies
+        dataManager = App.getDataManager()
+        notiManager = AppNotiManager.getInstance()
+    }
+
+    override fun createWork(): Single<Result> {
+        return Single.create { emitter ->
+            notiManager.showStoreSyncProgressStatusNotification(applicationContext.getString(R.string.str_updating_store_db), applicationContext.getString(R.string.str_update_app_db))
+
+            dataManager.loadStoresFromServer()
+                    .subscribe(object : SingleObserver<List<Store>> {
+                        override fun onSubscribe(d: Disposable) {
+                            Thread.sleep(5000)
+                        }
+
+                        override fun onSuccess(storeList: List<Store>) {
+                            val filteredStoreList = filterInvalidData(storeList.toMutableList())
+                            dataManager.setStores(filteredStoreList)
+
+                            if (!filteredStoreList.isEmpty()) {
+                                emitter.onSuccess(Result.success())
+                                notiManager.showStoreSyncStatusNotification(
+                                        String.format(applicationContext.getString(R.string.update_database_successfull_with_count), filteredStoreList.size),
+                                        applicationContext.getString(R.string.str_update_app_db))
+                                return
+                            }
+
+                            emitter.onSuccess(Result.failure())
+                        }
+
+                        override fun onError(e: Throwable) {
+                            emitter.onError(e)
+                        }
+                    })
+        }
+    }
+}
